fcfbfac2acd295a00267525c4c5bf940dece3e34
Tareq Malas <tmalas@lbl.gov>
2016-11-10 12:36:23 -0800

    added roofline analysis plot with example data

--- scripts/analysis/analysis_utils.py
+++ scripts/analysis/analysis_utils.py
@@ -12,8 +12,7 @@ def get_meta(l, d):
     """get meta data"""
     layer_re = re.compile('.+ Creating layer (.+)')
     m = layer_re.findall(l)
-    if(m is not []): layers = m
-    d['layers'] = set(layers)
+    if(m is not []): d['layers'] = m
 
     iters_re = re.compile('Testing for\s+(\d+)\s+iterations.')
     m = iters_re.search(l)
@@ -199,6 +198,12 @@ def plot_comparative(files_loc, threshold, res_path='', title_postfix='', sort_d
 def plot_pie(files_loc, threshold, res_path='', title_postfix='', sort_data=True):
     """Plot a time breakdown pie chart from a data frame record"""
     df = get_df(glb(files_loc))
+    
+    df['time per iteration'] = df['time per iteration'] - df['data forward avg time']
+    df['time per iteration'] = df['time per iteration'] - df['data backward avg time']
+    df.drop('data forward avg time', axis=1)
+    df.drop('data backward avg time', axis=1)
+
     df_filt = group_small_entries(df, threshold)
     df_filt = normalize_time(df_filt)
 
@@ -206,6 +211,8 @@ def plot_pie(files_loc, threshold, res_path='', title_postfix='', sort_data=True
     layers_cols = layers_cols + ['others time']    
     df_filt.index = df_filt['file name']
     plt_data = pd.DataFrame(df_filt[layers_cols].transpose(),columns=df_filt['file name'], index=layers_cols)
+    plt_data = plt_data[plt_data.index != 'data forward avg time']
+    plt_data = plt_data[plt_data.index != 'data backward avg time']
 
     if(sort_data):
         plt_data= plt_data.sort_index()
@@ -217,11 +224,22 @@ def plot_pie(files_loc, threshold, res_path='', title_postfix='', sort_data=True
     plt.savefig(os.path.join(res_path,'time_breakdown'+title_postfix+'.jpg'), format='jpg',bbox_inches='tight', dpi=900)
     return ax
 
-def plot_flops(sde_files_loc, time_file_loc, res_path='', title_postfix='', sort_data=True):
+def plot_flops(sde_files_loc, time_file_loc, res_path='', title_postfix='', sort_data=False, threshold=1.0):
     """Plot the flop rate given a set of results from SDE for each layer and the time experiement"""
     # get the timing results of the test case
     df = get_df(glb(time_file_loc))
 
+    layers = list(df['layers'].tolist()[0])
+    expanded_layers = []
+    for l in layers:
+        expanded_layers.append(l+' forward')
+        expanded_layers.append(l+' backward')
+    plt_df = pd.DataFrame(index=expanded_layers, columns=['Time', 'GFlops'])
+
+    layers_time_cols = {s.split(' avg')[0]:s for s in df.columns.values if('ward avg time' in s)}
+    for k,v in layers_time_cols.iteritems():
+        plt_df.loc[k,'Time'] = df[v].values[0]
+        
     # get the flops from the SDE tests
     for f in glb(sde_files_loc):
         with open(f, 'r') as fp:
@@ -229,17 +247,19 @@ def plot_flops(sde_files_loc, time_file_loc, res_path='', title_postfix='', sort
             txt = fp.read()
             get_meta(txt, entry)
             if ('profiled layer' in entry.keys()):
-                df[entry['profiled layer']+' gflops'] = entry['total flops']/df[entry['profiled layer']+ ' avg time']/1e9
+                plt_df.loc[entry['profiled layer'], 'GFlops'] = entry['total flops']/1e9
             else:
+                del plt_df[entry['profiled layer'], 'GFlops']
                 print "Could not parse: ", f
-    layers_gflops = [s for s in df.columns.values if('gflops' in s)]
-   
-    # layers_time_cols = [s for s in df.columns.values if('ward avg time' in s)] + ['others time']
-    layers_gflops_cols = [s for s in df.columns.values if('ward gflops' in s)]
-    
-    plt_data = pd.DataFrame(df[layers_gflops_cols].transpose(), index=layers_gflops_cols)
-    ax = plt_data.plot(kind='bar')
-    ax.set_title('Layers flop rate')
+
+    plt_df = plt_df[plt_df.index != 'data forward']
+    plt_df = plt_df[plt_df.index != 'data backward']
+    plt_df['time percent'] = 100.0*plt_df.Time/plt_df.Time.sum()
+
+    plt_filt = plt_df.loc[plt_df.loc[:,'time percent']>threshold,:]
+
+    ax = plt_filt.GFlops.plot(kind='bar')
+    ax.set_title('Layers flop rate'+' (filtered layers <'+str(threshold)+'% time)')
     ax.set_ylabel('GFLOP/s')
     ax.set_xlabel('Layer')
     plt.savefig(os.path.join(res_path,'flops_rate.jpg'), format='jpg',bbox_inches='tight', dpi=900)
@@ -270,3 +290,150 @@ def plot_all(f_wildcard, threshold=1.0, res_path=''):
             axis_l[arch][th] = plot_batch_scaling(batch_plts, threshold, threads=th, arch=arch, res_path=res_path)
     return axis_l
 
+def plot_roofline_points(points_list,res_path='',title_prefix='', labels_markers={}):
+    """Plot the KNL roofline for a set of data points
+        points_list=[(Label, AI, GFlop/s),]"""
+    from matplotlib import colors
+    def get_gflops_per_core(cpu_speed,num_cores,vector_length,fma):
+        fma_fac=2 if fma else 1
+        return cpu_speed*num_cores*vector_length*fma_fac
+
+    #Xeon Phi KNL, datasheet
+    cpu_speed_ghz=1.4
+    num_cores=68
+    vector_length=32
+    peak_bandwidth_gbs=90
+    peak_bandwidth_hbm_gbs=425
+    l1_cache_per_core_kb=32/2
+    l2_cache_per_core_kb=512/2
+    l3_cache_per_cpu_mb=0
+    peak_gflops_ixeknl=get_gflops_per_core(cpu_speed_ghz,num_cores,vector_length,True)
+
+    xopt = np.arange(0.1,300,0.01)
+    yopt = [np.min([val*peak_bandwidth_hbm_gbs,peak_gflops_ixeknl]) for val in xopt]
+    ynohbm = [np.min([val*peak_bandwidth_gbs,peak_gflops_ixeknl]) for val in xopt]
+    #ytlponly = [np.min([val*peak_bandwidth_hbm_gbs,get_gflops_per_core(cpu_speed_ghz,num_cores,1,False)]) for val in xopt]
+    #ytlpilp = [np.min([val*peak_bandwidth_hbm_gbs,get_gflops_per_core(cpu_speed_ghz,num_cores,2,False)]) for val in xopt]
+    #ynofma = [np.min([val*peak_bandwidth_hbm_gbs,get_gflops_per_core(cpu_speed_ghz,num_cores,vector_length,False)]) for val in xopt]
+    yfmanosimd = [np.min([val*peak_bandwidth_hbm_gbs,get_gflops_per_core(cpu_speed_ghz,num_cores,2,True)]) for val in xopt]
+
+    #removed malicious dpi=1024 argument
+    plt.figure(num=None, figsize=(20, 27), facecolor='w', edgecolor='k')
+    ax=plt.subplot(2,1,1)
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(50)
+
+    area = np.pi * 17**2
+
+    #ranges:
+    plt.xlim(10**(-1),max(xopt))
+    plt.ylim(10**1,10**4)
+
+    ax.tick_params(axis='both', which='major', size=15)
+    ax.tick_params(axis='both', which='minor', size=15)
+    ax.tick_params(axis='both', which='major', labelsize=50)
+
+    #lines
+    line1,=plt.plot(xopt, yopt, '-', linewidth=3,color='green',label='optimal SP (HBM)')
+    line2,=plt.plot(xopt, ynohbm, '-.', linewidth=5,color='k',label='optimal SP (DDR)')
+    #line3,=plt.plot(xopt, ynofma, '-', linewidth=3,color='orange',label='no FMA')
+    line3m,=plt.plot(xopt, yfmanosimd, '--', linewidth=3,color='blue',label='FMA no SIMD')
+    #line4,=plt.plot(xopt, ytlpilp, '-', linewidth=3,color='red',label='TLP with ILP')
+    #line4m,=plt.plot(xopt, ytlponly, '--', linewidth=3,color='red',label='TLP only')
+
+    
+    c_list = colors.cnames.values()
+    for ind, (label, x, y) in enumerate(points_list):
+        marker='v'
+        for k,v in labels_markers.iteritems():
+            if(k in label):
+                marker=v
+                break
+        ax.scatter(x,y,marker=marker, linewidth=5,s=area,alpha=0.5, label=label, color=c_list[ind])
+
+    for tick in ax.xaxis.get_major_ticks():
+        tick.set_pad(20)
+    for tick in ax.yaxis.get_major_ticks():
+        tick.set_pad(15)
+
+    plt.title(title_prefix, size=50, y=1.01)
+    ax.set_xscale('log',basex=10)
+    ax.set_yscale('log',basey=10)
+    ax.set_xlabel('Arithmetic Intensity Flops/Byte', size=50)
+    ax.set_ylabel('GFlops/s', size=50)
+    
+    ax.legend(scatterpoints=1, prop={'size':20}, loc='center left', bbox_to_anchor=(1, 0.5),title='layers end w/ time %')
+    ax.get_legend().get_title().set_fontsize('20')
+
+    plt.savefig(os.path.join(res_path,'roofline.jpg'), format='jpg',bbox_inches='tight', dpi=900)
+    return ax
+
+def generate_roofline(time_file_loc, sde_files_loc, likwid_file_loc, res_path=''
+                      ,sort_data=False, title_prefix='', threshold=1.):
+    df = get_df(glb(time_file_loc))
+    """Generate roofline figure from SDE, LIKWID, and timing measurements"""
+
+    layers = list(df['layers'].tolist()[0])
+    expanded_layers = []
+    for l in layers:
+        expanded_layers.append(l+' forward')
+        expanded_layers.append(l+' backward')
+    plt_df = pd.DataFrame(index=expanded_layers, columns=['Time', 'GFlops', 'GB memory volume'])
+
+    layers_time_cols = {s.split(' avg')[0]:s for s in df.columns.values if('ward avg time' in s)}
+    for k,v in layers_time_cols.iteritems():
+        plt_df.loc[k,'Time'] = df[v].values[0]
+
+    # Parse LIKWID memory test
+    f = glb(likwid_file_loc)[0]
+    with open(f, 'r') as fp:
+        likwid_entry = dict()
+        txt = fp.read()
+        get_meta(txt, likwid_entry)
+
+        # Get the measurement type
+        group_re = re.compile('TABLE,Region.*Metric,(\w*)')
+        m = group_re.search(txt)
+        if(m is not None): df['HW perf group'] = m.group(1)
+
+        for layer in layers:
+            mem_vol_re = re.compile('('+layer+')_(forward|backward).*?Memory data volume \[GBytes\],(\d+(.\d+)?)',re.DOTALL)
+            m = mem_vol_re.findall(txt)
+            if(m is not None):
+                for t in m:
+                    exp_layer = t[0]+' '+t[1]
+                    plt_df.loc[exp_layer, 'GB memory volume'] = float(t[2])/likwid_entry['iterations']
+
+    # get the flops from the SDE tests
+    for f in glb(sde_files_loc):
+        with open(f, 'r') as fp:
+            entry = dict()
+            txt = fp.read()
+            get_meta(txt, entry)
+            if ('profiled layer' in entry.keys()):
+                plt_df.loc[entry['profiled layer'], 'GFlops'] = entry['total flops']/1e9
+            else:
+                del plt_df[entry['profiled layer'], 'GFlops']
+                print "Could not parse: ", f
+
+
+    # Filter the data
+    plt_df = plt_df[plt_df.GFlops != 0.0]
+    plt_df = plt_df[plt_df.index != 'data forward']
+    plt_df = plt_df[plt_df.index != 'data backward']
+
+    # Add derived metrics of interest
+    plt_df['AI'] = plt_df['GFlops']/plt_df['GB memory volume']
+    plt_df['GFlop/s'] = plt_df.GFlops/plt_df.Time
+    plt_df['time percent'] = 100.0*plt_df.Time/plt_df.Time.sum()
+
+    plt_filt = plt_df.loc[plt_df.loc[:,'time percent']>threshold,:]
+
+    if(sort_data): plt_filt = plt_filt.sort_index()
+    plt_filt.index = plt_filt.index.str.cat(plt_filt['time percent'].map('{:02.0f}'.format), sep=' ')
+    data_points = zip(plt_filt.index.tolist(),plt_filt.AI.tolist(),plt_filt['GFlop/s'].tolist())
+
+    #for d in data_points: print d
+    ax = plot_roofline_points(data_points, res_path=res_path, title_prefix=title_prefix+' (filtered layers <'+str(threshold)+'%)'
+                       , labels_markers={'conv':'.', 'fc':'^', 'norm=':'x', 'pool':'*', 'loss':'v'})
+    return ax
